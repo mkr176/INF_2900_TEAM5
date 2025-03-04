1110068db8087bd7c3670fc446a902385ef66fd8
julius <ge35bun@mytum.de>
2025-03-04 10:46:04 +0100

    Update test execution scripts to use Django's manage.py for running tests

--- Windows_run_tests.bat
+++ Windows_run_tests.bat
@@ -1,6 +1,9 @@
 @echo off
 echo Activating Virtual Environment...
 call venv\Scripts\activate
+@REM python run_tests.py
+cd %~dp0
+cd library_manager
+python manage.py test
 
-python run_tests.py
 pause 
